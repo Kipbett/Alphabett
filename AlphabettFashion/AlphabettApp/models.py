1a7933a271b80569227b106dde275f205f5f9d1e
unknown <bettkipngenobrian@gmail.com>
2024-06-06 10:17:07 +0300

    Enabling bootstrap

--- AlphabettFashion/AlphabettApp/models.py
+++ AlphabettFashion/AlphabettApp/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-# class Images(models.Model):
-#     photo = models.ImageField(upload_to='photos/')
-
-
 class Product(models.Model):
 
     PRODUCT_CATEGORIES = (
@@ -21,8 +15,7 @@ class Product(models.Model):
     product_description = models.CharField(max_length=1000)
     product_selling_price = models.IntegerField()
     product_buying_price = models.IntegerField()
-    # photos = models.ForeignKey(Images, on_delete=models.CASCADE, related_name='product_images')
-    image = models.ImageField(upload_to='photos/')
+    image = models.ImageField(upload_to='photos/shoes')
     product_size = models.IntegerField()
     sub_category = models.CharField(max_length=100, choices=PRODUCT_CATEGORIES)
     product_stock = models.IntegerField()
@@ -39,7 +32,7 @@ class Watches(models.Model):
     
     product_name = models.CharField(max_length=100)
     product_color = models.CharField(max_length=50)
-    product_image = models.ImageField(upload_to='photos/')
+    product_image = models.ImageField(upload_to='photos/watches')
     product_buying_price = models.IntegerField()
     product_selling_price = models.IntegerField()
     product_gender = models.CharField(max_length=10, choices=GENDER_SELECTION)
@@ -58,7 +51,7 @@ class HomeAppliances(models.Model):
     )
 
     product_name = models.CharField(max_length=100)
-    product_image = models.ImageField(upload_to='photos/')
+    product_image = models.ImageField(upload_to='photos/home_appliances')
     product_buying_price = models.IntegerField()
     product_selling_price = models.IntegerField()
     product_category = models.CharField(max_length=20, choices=PRODUCT_SELECTION)
